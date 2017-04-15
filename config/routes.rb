2b7cc860ea8b68ff78c907bfe4f506f470afba5d
farheen <farheenf21@gmail.com>
2017-04-15 22:33:49 +0530

    intended

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	root 'student_tables#new'
- resources :student_tables
+ 	resources :student_tables
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
