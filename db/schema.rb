5512d235704f8f7f4627f55338274a89fe1fb75d
farheen <farheenf21@gmail.com>
2017-04-15 21:58:51 +0530

    gemfile changed to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170201170325) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "student_tables", force: :cascade do |t|
     t.string   "name"
     t.string   "semester"
