6954a16d2cdb94561009878f0fba0cd93732151e
farheen <farheenf21@gmail.com>
2017-04-23 11:32:33 +0530

    validation added

--- app/models/student_table.rb
+++ app/models/student_table.rb
@@ -1,2 +1,7 @@
 class StudentTable < ApplicationRecord
+	  validates :name, presence: true
+	  validates_format_of :phone_number, :with => /[0-9]{9}/ 
+	  validates  :email_id,format: {with: ~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message:"enter the email_id"}
+	  validates :address, presence: true
+
 end
